8b6aaab57f04d833285e3e6e0f64a571a8489d8d
knightaria <d.ketabdar@gmail.com>
2017-01-14 14:20:00 +0330

    akharin taghirat

--- app/src/main/java/ir/reserveiran/mobile/tourismapplication/MainActivity.java
+++ app/src/main/java/ir/reserveiran/mobile/tourismapplication/MainActivity.java
@@ -11,7 +11,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Intent i = new Intent(getApplicationContext(),HotelSearch.class);
-        startActivity(i);
+    // Intent i = new Intent(getApplicationContext(),HotelSearch.class);
+     // startActivity(i);
     }
 }
