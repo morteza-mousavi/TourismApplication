20efae4ce5b57e1a3938bf5a0feef7128e72cf10
knightaria <d.ketabdar@gmail.com>
2017-01-19 13:40:28 +0330

    new cheng

---
+++ app/src/main/java/ir/reserveiran/mobile/tourismapplication/airplaine.java
@@ -0,0 +1,13 @@
+package ir.reserveiran.mobile.tourismapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class airplaine extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_airplaine);
+    }
+}
