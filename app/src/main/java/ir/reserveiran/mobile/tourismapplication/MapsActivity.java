5f42218f609b1cd8013e2abfcb635ed291e1eb52
knightaria <d.ketabdar@gmail.com>
2017-01-28 11:36:10 +0330

    akharin taghirat

--- app/src/main/java/ir/reserveiran/mobile/tourismapplication/MapsActivity.java
+++ app/src/main/java/ir/reserveiran/mobile/tourismapplication/MapsActivity.java
@@ -3,6 +3,7 @@ package ir.reserveiran.mobile.tourismapplication;
 import android.content.Context;
 import android.content.pm.PackageManager;
 import android.location.Location;
+<<<<<<< Updated upstream
 import android.location.LocationListener;
 import android.location.LocationManager;
 import android.os.Build;
@@ -12,6 +13,23 @@ import android.os.Bundle;
 
 
 import com.google.android.gms.common.api.GoogleApiClient;
+=======
+import android.location.LocationManager;
+import android.os.Build;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.app.FragmentActivity;
+import android.os.Bundle;
+import android.support.v7.graphics.Palette;
+import android.view.View;
+import android.widget.Button;
+
+import com.google.android.gms.appindexing.Action;
+import com.google.android.gms.appindexing.AppIndex;
+import com.google.android.gms.appindexing.Thing;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.LocationListener;
+>>>>>>> Stashed changes
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
@@ -41,6 +59,7 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
             public void onLocationChanged(Location location) {
 
             }
+<<<<<<< Updated upstream
 
             @Override
             public void onStatusChanged(String provider, int status, Bundle extras) {
@@ -56,6 +75,8 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
             public void onProviderDisabled(String provider) {
 
             }
+=======
+>>>>>>> Stashed changes
         };
 
             SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
