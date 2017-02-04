3faefb6334e427cdea37b9ed40978ac314844ab3
knightaria <d.ketabdar@gmail.com>
2017-02-04 17:02:31 +0330

    last chenge

---
+++ app/src/main/java/ir/reserveiran/mobile/tourismapplication/RoomTypeActivity.java
@@ -0,0 +1,13 @@
+package ir.reserveiran.mobile.tourismapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class RoomTypeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_room_type);
+    }
+}
